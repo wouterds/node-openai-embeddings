ad9b2c1d154ac727ef7fab2aadee6f5047ac37d1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-13 18:48:08 +0200

    feat: helper methods to getVectors & add items to index

--- src/cli/generate-db.ts
+++ src/cli/generate-db.ts
@@ -1,6 +1,7 @@
 import colors from 'colors';
 import { config as dotenv } from 'dotenv';
 import fs from 'fs';
+import { Configuration, OpenAIApi } from 'openai';
 import { LocalIndex } from 'vectra';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
@@ -32,4 +33,32 @@ if (!fs.existsSync(csv)) {
   } else {
     console.log('Re-using existing index');
   }
+
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    }),
+  );
+
+  const getVector = async (text: string) => {
+    const response = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input: text,
+    });
+
+    return response.data.data[0].embedding;
+  };
+
+  const addItem = async (text: string) => {
+    try {
+      await index.insertItem({
+        vector: await getVector(text),
+        metadata: { text },
+      });
+
+      return true;
+    } catch {
+      return false;
+    }
+  };
 })();
