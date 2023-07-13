fc65a3e3c3e115eecce432eafa71d4ea6ffc7dd9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-13 18:55:23 +0200

    refactor: move to VectorDB class

---
+++ src/services/vector-db.ts
@@ -0,0 +1,65 @@
+import { Configuration, OpenAIApi } from 'openai';
+import { LocalIndex } from 'vectra';
+
+class VectorDB {
+  private _path: string;
+  private _index: LocalIndex;
+  private _openai: OpenAIApi | null = null;
+
+  constructor(path: string) {
+    this._path = path;
+    this._index = new LocalIndex(path);
+  }
+
+  public async init() {
+    const exists = await this._index.isIndexCreated();
+    if (exists) {
+      console.log(`Re-using existing index: ${this._path}`);
+      return;
+    }
+
+    console.log(`Creating new index: ${this._path}`);
+    await this._index.createIndex();
+  }
+
+  private get openai() {
+    if (!this._openai) {
+      this._openai = new OpenAIApi(
+        new Configuration({
+          apiKey: process.env.OPENAI_API_KEY,
+        }),
+      );
+    }
+
+    return this._openai;
+  }
+
+  public async getVector(text: string) {
+    try {
+      const response = await this.openai.createEmbedding({
+        model: 'text-embedding-ada-002',
+        input: text,
+      });
+
+      return response.data.data[0].embedding;
+    } catch {
+      return null;
+    }
+  }
+
+  public async addItem(text: string) {
+    const vector = await this.getVector(text);
+    if (!vector) {
+      return false;
+    }
+
+    try {
+      await this._index.insertItem({ vector, metadata: { text } });
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
+
+export default VectorDB;
