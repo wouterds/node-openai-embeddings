267519664bf77562b87a974d3c2c7b7eb0f79138
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-13 18:44:16 +0200

    feat: validate if csv exists

--- src/cli/generate-db.ts
+++ src/cli/generate-db.ts
@@ -1,5 +1,6 @@
 import colors from 'colors';
 import { config as dotenv } from 'dotenv';
+import fs from 'fs';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 
@@ -13,5 +14,10 @@ const { csv } = yargs(hideBin(process.argv)).argv as Args;
 
 if (!csv) {
   console.log(colors.red('Please provide a csv with the --csv flag'));
+  process.exit(1);
+}
+
+if (!fs.existsSync(csv)) {
+  console.log(colors.red(`File ${csv} does not exist`));
   process.exit(1);
 }
